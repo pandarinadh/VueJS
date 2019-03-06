341980ebc8ab1ab90925e9f65906babd9a1f3c2d
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2019-03-06 13:54:24 -0500

    single file component changes

---
+++ src/theme/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <nav class="nav has-shadow">
+        <div class="container">
+            <a href="/">
+            <img src="http://bit.ly/vue-img" alt ="Vue Project" />
+            </a>
+        </div>
+    </nav>
+    <section class="main-section section"></section>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                Follow us on
+                <a href="#" target="_blank">My Link</a>
+            </div>
+        </div>
+    </footer>
+</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+@import '~bulma'
+</style>
